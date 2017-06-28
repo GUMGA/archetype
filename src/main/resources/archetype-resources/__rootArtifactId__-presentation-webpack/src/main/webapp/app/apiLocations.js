b888c6176081d4a8396e2a572e9ca2d2b661c681
Mateus Miranda <info.mateusmiranda@gmail.com>
2017-06-28 17:27:22 -0300

    Webpack

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-presentation-webpack/src/main/webapp/app/apiLocations.js
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+'use strict';
+window.APILocation = {
+    apiLocation: location.origin+"/${parentArtifactId}-api"
+};
